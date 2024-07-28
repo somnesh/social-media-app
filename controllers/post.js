606a98423fd20901f6e73c3bd5a85a0796e5bf5b
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-07-28 11:57:43 +0530

    added 3 new api

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,16 @@
 const Post = require("../models/Post");
 const { StatusCodes } = require("http-status-codes");
-const { NotFoundError } = require("../errors");
+const { NotFoundError, BadRequestError } = require("../errors");
+
+const getAllPost = async (req, res) => {
+  const posts = (
+    await Post.find({ user_id: req.user.userId }).sort("createdAt")
+  ).reverse();
+  if (!posts) {
+    throw new NotFoundError(`No post found`);
+  }
+  res.status(StatusCodes.OK).json({ count: posts.length, posts });
+};
 
 const createPost = async (req, res) => {
   req.body.user_id = req.user.userId;
@@ -18,14 +28,44 @@ const getPostDetails = async (req, res) => {
 };
 
 const updatePost = async (req, res) => {
-  res.send("Update post");
+  const {
+    params: { id: postId },
+    user: { userId },
+    body: { content },
+  } = req;
+  if (!content) {
+    throw new BadRequestError("Content field cannot be empty");
+  }
+  const post = await Post.findOneAndUpdate(
+    { _id: postId, user_id: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!post) {
+    throw new NotFoundError(
+      `The user ${req.user.name} has no post with id: ${postId}`
+    );
+  }
+  res.status(StatusCodes.OK).json({ post });
 };
 
 const deletePost = async (req, res) => {
-  res.send("Delete post");
+  const {
+    params: { id: postId },
+    user: { userId },
+  } = req;
+
+  const post = await Post.findOneAndDelete({ _id: postId, user_id: userId });
+  if (!post) {
+    throw new NotFoundError(
+      `The user ${req.user.name} has no post with id: ${postId}`
+    );
+  }
+  res.status(StatusCodes.OK).json({ post });
 };
 
 module.exports = {
+  getAllPost,
   createPost,
   getPostDetails,
   updatePost,
--- routes/post.js
+++ routes/post.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const {
+  getAllPost,
   createPost,
   getPostDetails,
   updatePost,
   deletePost,
 } = require("../controllers/post");
 
-router.route("/").post(createPost);
+router.route("/").get(getAllPost).post(createPost);
 router.route("/:id").get(getPostDetails).patch(updatePost).delete(deletePost);
 
 module.exports = router;
