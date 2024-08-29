1aacad207c878e0b6cbee7853787d3ba587c59af
Atanu Malik <111167455+AtanuMalik@users.noreply.github.com>
2024-08-29 22:51:55 +0530

    changes made on several files

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,23 +1,44 @@
 import { Post } from "./Post";
 
 export function Feed() {
-    return (
-      <section className="basis-1/2">
-        <div className="create-new-post">
-          <div className="flex">
-            <div className="basis-1/4">dp</div>
-            <div className="basis-3/4">what's happening</div>
+  return (
+    <section className="basis-1/2">
+      <div className="create-new-post bg-slate-100 px-4 py-2">
+        <div className="flex items-center">
+          <div className="basis-1/6 ">
+            <img
+              className="rounded-full m-auto"
+              src="https://via.placeholder.com/40"
+              alt="photo"
+            />
           </div>
-          <div className="flex">
-            <div className="basis-1/3">add image</div>
-            <div className="basis-1/3">add poll</div>
-            <div className="basis-1/3">more</div>
+          <div className="basis-5/6">what's happening?</div>
+        </div>
+
+        <div className="flex items-center text-center">
+          <div className="flex basis-1/3 items-center p-2">
+            <span className="material-symbols-outlined">
+              add_photo_alternate
+            </span>
+            <span>add image</span>
+          </div>
+          <div className="flex basis-1/3 items-center">
+            <span className="material-symbols-outlined">ballot</span>
+            <span>add poll</span>
+          </div>
+          <div className="flex basis-1/3 items-center">
+            <span className="material-symbols-outlined">
+              expand_circle_right
+            </span>
+            <span>more</span>
           </div>
         </div>
-        {/* Main feed starts from here */}
-        <div>
-            <Post/>
-        </div>
-      </section>
-    );
+      </div>
+
+      {/* Main feed starts from here */}
+      <div>
+        <Post />
+      </div>
+    </section>
+  );
 }
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,24 +1,24 @@
 export function Sidebar(params) {
   return (
-    <section className="basis-1/4">
+    <section className="dark:bg-slate-900 basis-1/4">
       <div className="flex items-center gap-1 p-2">
-        <span class="material-symbols-outlined">home</span>
+        <span className="material-symbols-outlined">home</span>
         <span>Home</span>
       </div>
       <div className="flex items-center gap-1 p-2">
-        <span class="material-symbols-outlined">group</span>
+        <span className="material-symbols-outlined">group</span>
         <span>Friends</span>
       </div>
       <div className="flex items-center gap-1 p-2">
-        <span class="material-symbols-outlined">group_add</span>
+        <span className="material-symbols-outlined">group_add</span>
         <span>Friend Requests</span>
       </div>
       <div className="flex items-center gap-1 p-2">
-        <span class="material-symbols-outlined">bookmark</span>
+        <span className="material-symbols-outlined">bookmark</span>
         <span>Saved</span>
       </div>
       <div className="flex items-center gap-1 p-2">
-        <span class="material-symbols-outlined">diamond</span>
+        <span className="material-symbols-outlined">diamond</span>
         <span>Premium</span>
       </div>
     </section>
