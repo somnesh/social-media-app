3fa411e804df57f6715d0f180b60fc33b7ceba4f
Atanu Malik <111167455+AtanuMalik@users.noreply.github.com>
2024-08-29 22:08:06 +0530

    changes added in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,15 +2,22 @@ import { SearchBar } from "./SearchBar";
 
 export function Header() {
   return (
-    <header className="flex p-2 bg-[#242526]">
+    <header className="flex p-2 bg-[#242526] items-center">
       <section className="basis-1/4">
         <SearchBar />
       </section>
       <section className="basis-1/2 text-center">
-        <h2>logo</h2>
+        <h2>
+          <span className="material-symbols-outlined text-white">token</span>
+        </h2>
       </section>
-      <section className="basis-1/4 text-right">
-        <h2>Profile</h2>
+      <section className="flex basis-1/4 justify-end items-center gap-2">
+        <h2 className="text-white">Profile</h2>
+        <img
+          className="rounded-full"
+          src="https://via.placeholder.com/40"
+          alt="photo"
+        />
       </section>
     </header>
   );
