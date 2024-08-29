8a2e95439c9f5fbbc2d1ed7291dcb0c2242b5cde
Atanu Malik <111167455+AtanuMalik@users.noreply.github.com>
2024-08-29 22:08:20 +0530

    Sidebar modified

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,11 +1,26 @@
 export function Sidebar(params) {
-    return (
-      <section className="basis-1/4">
-        <div>Home</div>
-        <div>Friends</div>
-        <div>Requests</div>
-        <div>Saved</div>
-        <div>Premium</div>
-      </section>
-    );
+  return (
+    <section className="basis-1/4">
+      <div className="flex items-center gap-1 p-2">
+        <span class="material-symbols-outlined">home</span>
+        <span>Home</span>
+      </div>
+      <div className="flex items-center gap-1 p-2">
+        <span class="material-symbols-outlined">group</span>
+        <span>Friends</span>
+      </div>
+      <div className="flex items-center gap-1 p-2">
+        <span class="material-symbols-outlined">group_add</span>
+        <span>Friend Requests</span>
+      </div>
+      <div className="flex items-center gap-1 p-2">
+        <span class="material-symbols-outlined">bookmark</span>
+        <span>Saved</span>
+      </div>
+      <div className="flex items-center gap-1 p-2">
+        <span class="material-symbols-outlined">diamond</span>
+        <span>Premium</span>
+      </div>
+    </section>
+  );
 }
