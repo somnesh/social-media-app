eed4595882dd2f3b0998ed6fa6994b0a8bb5fb20
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-08-31 22:15:03 +0530

    Design updated

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,35 +1,34 @@
+import { CirclePlus, ImagePlus } from "lucide-react";
 import { Post } from "./Post";
 
 export function Feed() {
   return (
-    <section className="basis-1/2 mx-16">
-      <div className="create-new-post bg-white dark:bg-[#242526] px-4 py-2">
-        <div className="flex items-center">
-          <div className="basis-1/6 ">
+    <section className="basis-1/2 mx-auto max-w-[680px] select-none gap-2">
+      <div className="create-new-post bg-white dark:bg-[#242526] px-4 py-2 rounded-lg">
+        <div className="flex items-center py-2 gap-3">
+          <div className="">
             <img
-              className="rounded-full m-auto"
-              src="https://via.placeholder.com/40"
+              className="rounded-full "
+              src="https://via.placeholder.com/50"
               alt="photo"
             />
           </div>
-          <div className="basis-5/6">what's happening?</div>
+          <div className=" bg-[#414141] active:bg-[#6e6e6e] hover:bg-[#535353] rounded-lg py-3 px-4 cursor-pointer w-full">
+            what's happening?
+          </div>
         </div>
 
-        <div className="flex items-center text-center">
-          <div className="flex basis-1/3 items-center p-2">
-            <span className="material-symbols-outlined">
-              add_photo_alternate
-            </span>
+        <div className="flex items-center text-center pt-2">
+          <div className="flex basis-1/3 items-center justify-center p-2 gap-1 cursor-pointer hover:bg-[#414141] rounded-md">
+            <ImagePlus />
             <span>add image</span>
           </div>
-          <div className="flex basis-1/3 items-center">
+          <div className="flex basis-1/3 items-center justify-center p-2 gap-1 cursor-pointer hover:bg-[#414141] rounded-md">
             <span className="material-symbols-outlined">ballot</span>
             <span>add poll</span>
           </div>
-          <div className="flex basis-1/3 items-center">
-            <span className="material-symbols-outlined">
-              expand_circle_right
-            </span>
+          <div className="flex basis-1/3 items-center justify-center p-2 gap-1 cursor-pointer hover:bg-[#414141] rounded-md">
+            <CirclePlus />
             <span>more</span>
           </div>
         </div>
