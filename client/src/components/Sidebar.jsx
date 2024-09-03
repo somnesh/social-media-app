e840b3551c06a3d0b2b8fd0c595faf2b6e36dad6
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-09-03 21:34:13 +0530

    position sticky added

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,25 +2,25 @@ import { Bookmark, Gem, Home, UserPlus, Users } from "lucide-react";
 
 export function Sidebar(params) {
   return (
-    <section className="dark:bg-[18191a] basis-1/4">
-      <div className="flex items-center gap-1 p-2">
-        <Home/>
+    <section className="  basis-1/4 h-screen sticky top-[4.5rem] overflow-y-auto rounded-md gap-1 mr-auto max-w-80">
+      <div className="flex items-center gap-1 p-3 hover:bg-[#e3e5e9] dark:hover:bg-[#414141] cursor-pointer rounded-md">
+        <Home />
         <span>Home</span>
       </div>
-      <div className="flex items-center gap-1 p-2">
-        <Users/>
+      <div className="flex items-center gap-1 p-3 hover:bg-[#e3e5e9] dark:hover:bg-[#414141] cursor-pointer rounded-md">
+        <Users />
         <span>Friends</span>
       </div>
-      <div className="flex items-center gap-1 p-2">
-        <UserPlus/>
+      <div className="flex items-center gap-1 p-3 hover:bg-[#e3e5e9] dark:hover:bg-[#414141] cursor-pointer rounded-md">
+        <UserPlus />
         <span>Friend Requests</span>
       </div>
-      <div className="flex items-center gap-1 p-2">
-        <Bookmark/>
+      <div className="flex items-center gap-1 p-3 hover:bg-[#e3e5e9] dark:hover:bg-[#414141] cursor-pointer rounded-md">
+        <Bookmark />
         <span>Saved</span>
       </div>
-      <div className="flex items-center gap-1 p-2">
-        <Gem/>
+      <div className="flex items-center gap-1 p-3 hover:bg-[#e3e5e9] dark:hover:bg-[#414141] cursor-pointer rounded-md">
+        <Gem />
         <span>Premium</span>
       </div>
     </section>
