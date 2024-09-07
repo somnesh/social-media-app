dc9bf61c3534b0f356812cb102e33de172ecadc1
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-09-07 15:11:55 +0530

    added file upload middleware to the route

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const upload = require("../middleware/multer-middleware");
+const { uploadFile } = require("../middleware/uploadFile");
+
 const {
   getAllPost,
   createPost,
@@ -9,7 +12,10 @@ const {
   deletePost,
 } = require("../controllers/post");
 
-router.route("/").get(getAllPost).post(createPost);
+router
+  .route("/")
+  .get(getAllPost)
+  .post(upload.single("files")  , uploadFile, createPost);
 router.route("/:id").get(getPostDetails).patch(updatePost).delete(deletePost);
 
 module.exports = router;
