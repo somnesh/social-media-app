ae6fd7d61ca2f20dbfb4a8e15ce8484f6d688258
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-09-07 14:57:00 +0530

    can fetch post data from database

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,6 +1,67 @@
-import { Globe, Heart, MessageCircle, Repeat2 } from "lucide-react";
+import axios from "axios";
+import { Globe, Heart, Lock, MessageCircle, Repeat2, Users } from "lucide-react";
+import { useEffect, useState } from "react";
 
-export function Post() {
+export function Post({ details }) {
+  const [userInfo, setUserInfo] = useState([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/api/v1/user/${details.user_id}`,
+          {
+            headers: {
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NmE0YTViNzZmZWNhM2JmNGU0OTA1ZmUiLCJuYW1lIjoiam9obiBkb2UiLCJyb2xlIjoidXNlciIsImF2YXRhciI6bnVsbCwiaWF0IjoxNzI1Njk1MDIwLCJleHAiOjE3MjU3ODE0MjB9.mP6kU-iTTNaM30LsH17dZHiebJ_Xxcc9NsXA9MzzPi4",
+            },
+          }
+        );
+        setUserInfo(response.data);
+        // console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  // console.log(details.visibility);
+  
+  const postDate = new Date(details.createdAt);
+  const currentDate = new Date();
+
+  const diffInMilliseconds = currentDate - postDate;
+
+  const diffInSeconds = Math.floor(diffInMilliseconds / 1000);
+  const diffInMinutes = Math.floor(diffInMilliseconds / (1000 * 60));
+  const diffInHours = Math.floor(diffInMilliseconds / (1000 * 60 * 60));
+  const diffInDays = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
+
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let postDuration = "";
+  if (diffInSeconds < 60) {
+    postDuration = `Just now`;
+  } else if (diffInMinutes < 60) {
+    postDuration = `${diffInMinutes} minutes ago`;
+  } else if (diffInHours < 60) {
+    postDuration = `${diffInHours} hours ago`;
+  } else if (diffInDays < 30) {
+    postDuration = `${diffInDays} days ago`;
+  } else {
+    postDuration = `${postDate.getDate()} ${monthNames[postDate.getMonth()]}`;
+  }
   return (
     <div className="bg-white dark:bg-[#242526] px-4 py-2 rounded-lg mb-4">
       <div className="flex items-center gap-3 pt-2">
@@ -12,22 +73,24 @@ export function Post() {
           />
         </div>
         <div className="">
-          <h2 className="font-medium">Atanu Malik</h2>
+          <h2 className="font-medium">{userInfo.name}</h2>
           <div className="flex gap-1 items-center">
             <span>
-              <Globe size={16} strokeWidth={1.25} />
+              {details.visibility === "public" ? (
+                <Globe size={16} strokeWidth={1.25} />
+              ) : details.visibility === "friends" ? (
+                <Users size={16} strokeWidth={1.25} />
+              ) : (
+                <Lock size={16} strokeWidth={1.25} />
+              )}
             </span>
-
-            <span>2hrs</span>
+            <span>{postDuration}</span>
           </div>
         </div>
       </div>
       <div className="flex flex-col py-2">
-        <div className="pb-2">caption</div>
-        <img
-          src="https://yt3.googleusercontent.com/g3j3iOUOPhNxBCNAArBqiYGzHzCBIzr_Al8mdvtBJeZMGFDblnU5rlVUt6GY01AUwm7Cp70J=s900-c-k-c0x00ffffff-no-rj"
-          alt="photo"
-        />
+        <div className="pb-2">{details.content}</div>
+        {details.image_url && <img src={details.image_url} alt="photo" />}
       </div>
       <div className="flex py-1 border-y dark:dark:border-y-[#3a3b3c]">
         <div className="flex basis-1/3 justify-center transition delay-75 ease-in gap-1 p-2 hover:bg-[#f9188110] hover:text-[#f91880] cursor-pointer rounded-md">
