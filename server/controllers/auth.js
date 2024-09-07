1c35a18c4c4f3670d9afa3d4936e1593e3a89ee6
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-09-07 15:03:46 +0530

    added email verification

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,17 +1,77 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, UnauthenticatedError } = require("../errors");
+const sendVerificationEmail = require("../utils/sendVerificationEmail");
+
+const isUsernameUnique = async (username) => {
+  const isUnique = await User.findOne({ username });
+  if (isUnique) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
 // for new user registration
 const registerUser = async (req, res) => {
-  const { phone_no } = req.body;
-  req.body.phone_no = Number(phone_no);
+  const { name, username, email } = req.body;
+  let { phone_no } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new BadRequestError("Email already exists");
+  }
+
+  if (!isUsernameUnique(username)) {
+    throw new BadRequestError("Username already exists");
+  }
+
+  phone_no = Number(phone_no);
+  const phoneNoAlreadyExists = await User.findOne({ phone_no });
+  if (phoneNoAlreadyExists) {
+    throw new BadRequestError("Phone No. already exists");
+  }
+  req.body.phone_no = phone_no;
+
+  const verificationToken = "1234";
+  const origin = "http://localhost:3000/api/v1/";
+  await sendVerificationEmail({
+    name,
+    email,
+    verificationToken,
+    origin,
+  });
 
   // Inserting data into the database
-  const user = await User.create({ ...req.body });
-  const token = user.createJWT();
+  const user = await User.create({
+    ...req.body,
+    verificationToken: verificationToken,
+  });
 
-  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ msg: "Success! Please check your email to verify account" });
+};
+
+const verifyEmail = async (req, res) => {
+  const { token: verificationToken, email } = req.query;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError("Verification Failed");
+  }
+
+  if (user.verificationToken !== verificationToken) {
+    throw new UnauthenticatedError("Verification Failed");
+  }
+
+  user.isVerified = true;
+  user.verified = Date.now();
+  user.verificationToken = "";
+
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: "Email Verified" });
 };
 
 // for user login
@@ -35,10 +95,11 @@ const loginUser = async (req, res) => {
 
   //   when both credentials are correct a Json token is created for the session
   const token = user.createJWT();
-  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+  res.status(StatusCodes.OK).json({ user: { name: user.name, id: user._id }, token });
 };
 
 module.exports = {
   registerUser,
   loginUser,
+  verifyEmail,
 };
