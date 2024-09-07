75f46da4c8be0fd2efe1a8c72dbc8846314d19e2
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-09-07 15:03:22 +0530

    added admin login controller

---
+++ server/controllers/admin.js
@@ -0,0 +1,36 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+
+const {
+  BadRequestError,
+  UnauthenticatedError,
+  AccessDeniedError,
+} = require("../errors");
+
+const adminLogin = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid credential");
+  }
+
+  if (user.role !== "admin") {
+    throw new AccessDeniedError("Access Denied");
+  }
+
+  //   matching the password
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid credential");
+  }
+
+  //   when both credentials are correct a Json token is created for the session
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+};
+
+module.exports = { adminLogin };
