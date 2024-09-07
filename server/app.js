0c41cb50d998e7e947f2c849db62a2777a87aade
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-09-07 15:00:56 +0530

    added some routes and done minor changes

--- server/app.js
+++ server/app.js
@@ -5,10 +5,21 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 
+// middleware
+app.use(express.static("./public"));
+app.use(express.json());
+// cors middleware
+app.use(cors());
+
+// app.use(express.urlencoded({ extended: true }));
+
 // database connection handler
 const connectDB = require("./db/connect");
 
+// authentication middlewares
 const authenticateUser = require("./middleware/authentication");
+const authenticateAdmin = require("./middleware/admin-auth");
+
 // error handlers
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -16,18 +27,17 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 // routers
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/post");
-
-// middleware
-app.use(express.static("./public"));
-app.use(express.json());
-
-// cors middleware
-app.use(cors());
+const userRoute = require("./routes/user");
+const adminRoute = require("./routes/admin");
 
 // routes
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/post", authenticateUser, postRoute);
+app.use("/api/v1/user", authenticateUser, userRoute);
+app.use("/api/v1/auth/admin", adminRoute);
+app.use("/api/v1/admin", authenticateAdmin, adminRoute);
 
+// error handler middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
