192595acbf0484ad170ffe4adc6018c905b23bf6
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-09-07 15:11:07 +0530

    added email verification route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { loginUser, registerUser } = require("../controllers/auth");
+const { loginUser, registerUser, verifyEmail } = require("../controllers/auth");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/user/verify-email").post(verifyEmail);
 
 module.exports = router;
