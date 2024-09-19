c9c56a2e15e4b4b07ad4c2bb3a1d80d67d9895fa
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-09-19 15:49:41 +0530

    added authentication before rendering the page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,19 +1,55 @@
+import { useEffect, useState } from "react";
 import { Feed } from "../components/Feed";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { WholePageLoader } from "../components/loaders/WholePageLoader";
 
 export const Home = () => {
+  const [pageLoading, setPageLoading] = useState(true);
+  const navigate = useNavigate();
+
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setPageLoading(true);
+
+        const response = await axios.post(
+          `${API_URL}/auth/refresh-token`,
+          {},
+          {
+            withCredentials: true,
+          }
+        );
+
+        setPageLoading(false);
+      } catch (error) {
+        console.error(error);
+        navigate("/login");
+        setPageLoading(false);
+      }
+    })();
+  }, [navigate]);
   return (
     <>
-      <Header />
-      <main className="flex px-2 py-4 bg-[#f0f2f5] dark:bg-[#18191a] min-h-screen dark:text-[#e2e4e9] transition-colors duration-500">
-        <Sidebar />
-        <Feed />
-        {/* --------------------------------------- */}
-        <section className="basis-1/4">
-          <h2>unknown section</h2>
-        </section>
-      </main>
+      {pageLoading ? (
+        <WholePageLoader />
+      ) : (
+        <>
+          <Header />
+          <main className="flex px-2 py-4 bg-[#f0f2f5] dark:bg-[#18191a] min-h-screen dark:text-[#e2e4e9] transition-colors duration-500">
+            <Sidebar />
+            <Feed />
+            {/* --------------------------------------- */}
+            <section className="basis-1/4">
+              <h2>unknown section</h2>
+            </section>
+          </main>
+        </>
+      )}
     </>
   );
 };
