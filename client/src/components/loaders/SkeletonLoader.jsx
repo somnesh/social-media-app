4a6f749bc7f8cafdf01d778c57d7fab425af5223
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-09-19 15:31:25 +0530

    moved some files

---
+++ client/src/components/loaders/SkeletonLoader.jsx
@@ -0,0 +1,14 @@
+import { Skeleton } from "../ui/skeleton";
+
+export function SkeletonLoader({
+  width = "100%",
+  height = "20px",
+  className = "",
+}) {
+  return (
+    <Skeleton
+      className={`bg-gray-300 ${className}`}
+      style={{ width, height }}
+    />
+  );
+}
