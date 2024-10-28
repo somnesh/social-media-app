a31990ed102dc41fcbe87afc21cb2f3056866622
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-10-28 21:36:59 +0530

    added dislike route

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { generateFeed, logPostView } = require("../controllers/feed");
+const {
+  generateFeed,
+  logPostView,
+  dislikePostRecommendation,
+} = require("../controllers/feed");
 
 router.route("/").get(generateFeed);
 router.route("/log-post-view").post(logPostView);
+router.route("/post/dislike").post(dislikePostRecommendation);
 
 module.exports = router;
