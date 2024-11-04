6e3f1a94b26d4bb64a6102d5afb117f46578a28b
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-11-04 22:44:09 +0530

    added comment reply route

--- server/routes/UnprotectedPostRoutes.js
+++ server/routes/UnprotectedPostRoutes.js
@@ -5,11 +5,13 @@ const {
   getPostDetails,
   getLikes,
   getComments,
+  getCommentReplies,
 } = require("../controllers/post");
 const decryptUrl = require("../middleware/url-Decryption");
 
 router.route("/:id").get(decryptUrl, getPostDetails);
 router.route("/like/:id").get(getLikes);
 router.route("/comment/:id").get(getComments); // ":id" -> here "id" is "post id"
+router.route("/comment/:id/reply").get(getCommentReplies);
 
 module.exports = router;
