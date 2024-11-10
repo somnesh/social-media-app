247c3fa67b8b8720fb2f7f82a99ca5bd16543b41
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-11-10 19:07:44 +0530

    added feature to redirect to login page

--- client/src/pages/SomethingWentWrong.jsx
+++ client/src/pages/SomethingWentWrong.jsx
@@ -1,6 +1,23 @@
 import { BadgeAlert } from "lucide-react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function SomethingWentWrong() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const isPageReloaded = sessionStorage.getItem("reloaded");
+
+    if (isPageReloaded) {
+      // if the page is reloaded then it will redirect the page to login page
+      navigate("/login");
+    } else {
+      sessionStorage.setItem("reloaded", "true");
+    }
+
+    return () => {
+      sessionStorage.removeItem("reloaded");
+    };
+  }, []);
   return (
     <div className="h-screen flex flex-col justify-center items-center text-3xl gap-2">
       <div className="flex gap-2 font-semibold items-center text-red-500">
