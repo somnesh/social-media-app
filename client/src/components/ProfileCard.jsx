c165e3cae1f84f9a2df355d1e0ea7996929f7a95
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-11-10 19:13:02 +0530

    added profile card component

---
+++ client/src/components/ProfileCard.jsx
@@ -0,0 +1,151 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "./ui/button";
+import axios from "axios";
+import { useToastHandler } from "../contexts/ToastContext";
+import { useState } from "react";
+import { CircleCheck, CircleX } from "lucide-react";
+
+export function ProfileCard({ details, followingBack }) {
+  const [isFollowingBack, setIsFollowingBack] = useState(followingBack);
+  const [unfollowFlag, setUnfollowFlag] = useState(false);
+  const [followerRemovedFlag, setFollowerRemovedFlag] = useState(false);
+
+  const toastHandler = useToastHandler();
+  const API_URL = import.meta.env.VITE_API_URL;
+  console.log(details);
+
+  const handleUnfollow = async () => {
+    try {
+      const response = await axios.delete(
+        `${API_URL}/user/unfollow/${details._id}`,
+        { withCredentials: true }
+      );
+
+      if (followingBack !== null) {
+        setIsFollowingBack(false);
+      }
+      setUnfollowFlag(true);
+      toastHandler(
+        <div className="flex gap-2 items-center">
+          <CircleCheck className="bg-green-600 rounded-full text-white dark:text-[#242526]" />
+          <span>User unfollowed</span>
+        </div>,
+        false
+      );
+    } catch (error) {
+      console.error("Unfollow Failed: ", error);
+      toastHandler(
+        <div className="flex gap-2 items-center">
+          <CircleX className="bg-red-600 rounded-full text-white dark:text-[#7f1d1d]" />
+          <span>Something went wrong</span>
+        </div>,
+        true
+      );
+    }
+  };
+
+  const handleFollow = async () => {
+    try {
+      const response = await axios.post(
+        `${API_URL}/user/follow/${details._id}`,
+        {},
+        { withCredentials: true }
+      );
+
+      if (followingBack !== null) {
+        setIsFollowingBack(true);
+      }
+      setUnfollowFlag(false);
+      toastHandler(
+        <div className="flex gap-2 items-center">
+          <CircleCheck className="bg-green-600 rounded-full text-white dark:text-[#242526]" />
+          <span>User followed</span>
+        </div>,
+        false
+      );
+    } catch (error) {
+      console.error("Follow Failed: ", error);
+      toastHandler(
+        <div className="flex gap-2 items-center">
+          <CircleX className="bg-red-600 rounded-full text-white dark:text-[#7f1d1d]" />
+          <span>Something went wrong</span>
+        </div>,
+        true
+      );
+    }
+  };
+
+  const handleRemoveFollower = async () => {
+    try {
+      const response = await axios.delete(
+        `${API_URL}/user/remove-follower/${details._id}`,
+        { withCredentials: true }
+      );
+
+      setFollowerRemovedFlag(true);
+      toastHandler(
+        <div className="flex gap-2 items-center">
+          <CircleCheck className="bg-green-600 rounded-full text-white dark:text-[#242526]" />
+          <span>Follower removed</span>
+        </div>,
+        false
+      );
+    } catch (error) {
+      console.error(error);
+
+      toastHandler(
+        <div className="flex gap-2 items-center">
+          <CircleX className="bg-red-600 rounded-full text-white dark:text-[#7f1d1d]" />
+          <span>Something went wrong</span>
+        </div>,
+        true
+      );
+    }
+  };
+  return (
+    <>
+      {details && (
+        <div className="flex basis-1/2 flex-col gap-5 bg-white dark:bg-[#242526] px-4 py-3 transition-all drop-shadow-sm rounded-md">
+          <div className="flex">
+            <a href="#" className="flex gap-3 cursor-pointer items-center">
+              <Avatar>
+                <AvatarImage src={details?.avatar} />
+                <AvatarFallback className={details?.avatarBg}>
+                  {details?.name[0]}
+                </AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col">
+                <span className="font-medium hover:underline">
+                  {details?.name}
+                </span>
+                <span className="text-sm">{details?.profile_bio}</span>
+              </div>
+            </a>
+          </div>
+          <div className="flex gap-2 justify-end">
+            {isFollowingBack === null ? (
+              <>
+                {!unfollowFlag ? (
+                  <Button onClick={handleUnfollow}>Unfollow</Button>
+                ) : (
+                  <Button onClick={handleFollow}>Follow</Button>
+                )}
+              </>
+            ) : (
+              <>
+                {!followerRemovedFlag && (
+                  <Button onClick={handleRemoveFollower}>Remove</Button>
+                )}
+                {isFollowingBack ? (
+                  <Button onClick={handleUnfollow}>Unfollow</Button>
+                ) : (
+                  <Button onClick={handleFollow}>Follow back</Button>
+                )}
+              </>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
