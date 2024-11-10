9f20bb425275f13d48fcfc6e2d5ab593ab7ed15a
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-11-10 19:11:47 +0530

    added follower and following list

---
+++ client/src/components/FollowerList.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import { ProfileCard } from "./ProfileCard";
+import axios from "axios";
+import { ProfileCardLoader } from "./loaders/ProfileCardLoader";
+
+export function FollowerList() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [followerList, setFollowerList] = useState([]);
+  const [followerListSkip, setFollowerListSkip] = useState(0);
+  const [totalCount, setTotalCount] = useState(0);
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  const chunkArray = (array, size) => {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size));
+    }
+    return result;
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    (async () => {
+      try {
+        const response = await axios.get(`${API_URL}/user/fetch/followerList`, {
+          params: { limit: 10, skip: followerListSkip },
+          withCredentials: true,
+        });
+
+        setTotalCount(response.data.totalCount);
+
+        console.log(
+          "FollowerList.jsx : response.data: ",
+          response.data.followers
+        );
+        setFollowerListSkip((prev) => prev + response.data.length);
+
+        const followerChunks = chunkArray(response.data.followers, 2);
+        console.log("followerChunks: ", followerChunks);
+
+        setFollowerList(followerChunks);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        setIsLoading(false);
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <ProfileCardLoader />
+      ) : (
+        <div className="">
+          <div className="mb-4">
+            <span className="text-2xl font-bold">Followers ({totalCount})</span>
+          </div>
+          <div className="flex items-center flex-col gap-3">
+            {totalCount !== 0 ? (
+              followerList.map((chunk, index) => (
+                <div className="flex w-full gap-3" key={index}>
+                  {console.log("chunk: ", chunk)}
+                  {chunk.map((follower) => (
+                    <ProfileCard
+                      key={follower._id}
+                      details={follower.follower}
+                      followingBack={follower.followingBack}
+                    />
+                  ))}
+                </div>
+              ))
+            ) : (
+              <span className="block mt-10 font-medium text-center">
+                You don't have any followers right now, start following your
+                friends and family.
+              </span>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
---
+++ client/src/components/FollowingList.jsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+import { ProfileCard } from "./ProfileCard";
+import axios from "axios";
+import { ProfileCardLoader } from "./loaders/ProfileCardLoader";
+
+export function FollowingList() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [followingList, setFollowingList] = useState([]);
+  const [followingListSkip, setFollowingListSkip] = useState(0);
+  const [totalCount, setTotalCount] = useState(0);
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  const chunkArray = (array, size) => {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size));
+    }
+    return result;
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    (async () => {
+      try {
+        const response = await axios.get(
+          `${API_URL}/user/fetch/followingList`,
+          {
+            params: { limit: 10, skip: followingListSkip },
+            withCredentials: true,
+          }
+        );
+
+        setTotalCount(response.data.totalCount);
+
+        console.log(
+          "FollowerList.jsx : response.data: ",
+          response.data.following
+        );
+        setFollowingListSkip((prev) => prev + response.data.length);
+
+        const followingChunks = chunkArray(response.data.following, 2);
+        console.log("followingChunks: ", followingChunks);
+
+        setFollowingList(followingChunks);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        setIsLoading(false);
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <ProfileCardLoader />
+      ) : (
+        <div className="">
+          <div className="mb-4">
+            <span className="text-2xl font-bold">Following ({totalCount})</span>
+          </div>
+          <div className="flex items-center flex-col gap-3">
+            {totalCount !== 0 ? (
+              followingList.map((chunk, index) => (
+                <div className="flex w-full gap-3" key={index}>
+                  {console.log("chunk: ", chunk)}
+                  {chunk.map((follower) => (
+                    <ProfileCard
+                      key={follower._id}
+                      details={follower.followed}
+                      followingBack={null}
+                    />
+                  ))}
+                </div>
+              ))
+            ) : (
+              <span className="block mt-10 font-medium text-center">
+                You don't follow anyone right now, start following your friends
+                and family.
+              </span>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
