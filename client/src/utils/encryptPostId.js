9fb68965b854cbfb8d1193d5d6a7bd02b327fb9a
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-11-10 19:01:19 +0530

    helper function to encrypt id

---
+++ client/src/utils/encryptPostId.js
@@ -0,0 +1,16 @@
+import CryptoJS from "crypto-js";
+const encryptPostId = (id) => {
+  const ENCRYPTION_SECRET = import.meta.env.VITE_URL_ENCRYPTION_SECRET;
+  const encryptedPostID = CryptoJS.AES.encrypt(
+    id,
+    ENCRYPTION_SECRET
+  ).toString();
+
+  const urlSafeEncryptedPostID = encryptedPostID
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_");
+
+  return urlSafeEncryptedPostID;
+};
+
+export default encryptPostId;
