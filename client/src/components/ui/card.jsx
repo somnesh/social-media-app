8dba9969f0c69a9644530e474d03d7578cc4161e
Atanu Malik <111167455+AtanuMalik@users.noreply.github.com>
2024-11-11 23:07:18 +0530

    Imported card and tab component from shadcn

--- client/src/components/ui/card.jsx
+++ client/src/components/ui/card.jsx
@@ -5,10 +5,7 @@ import { cn } from "@/lib/utils";
 const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
-      className
-    )}
+    className={cn("border bg-card text-card-foreground shadow-sm", className)}
     {...props}
   />
 ));
@@ -24,7 +21,7 @@ const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
 CardHeader.displayName = "CardHeader";
 
 const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
-  <h3
+  <div
     ref={ref}
     className={cn(
       "text-2xl font-semibold leading-none tracking-tight",
@@ -36,7 +33,7 @@ const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
 CardTitle.displayName = "CardTitle";
 
 const CardDescription = React.forwardRef(({ className, ...props }, ref) => (
-  <p
+  <div
     ref={ref}
     className={cn("text-sm text-muted-foreground", className)}
     {...props}
