7c2b0303a3e7f4d4edd254781219cf91d4c84772
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-11-19 13:22:29 +0530

    Added responsiveness: made the header mobile device friendly

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,54 +1,70 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ProfileMenu } from "./ProfileMenu";
 import { SearchBar } from "./SearchBar";
 import { Button } from "./ui/button";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useAuth } from "../contexts/AuthContext";
+import { SkeletonLoader } from "./loaders/SkeletonLoader";
 
 export function Header() {
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
   const { setIsAuthenticated, isAuthenticated } = useAuth();
 
   const API_URL = import.meta.env.VITE_API_URL;
-  // useEffect(() => {
-  //   const refreshAuthToken = async () => {
-  //     try {
-  //       await axios.post(
-  //         `${API_URL}/auth/refresh-token`,
-  //         {},
-  //         {
-  //           withCredentials: true,
-  //         }
-  //       );
-  //       setIsAuthenticated(true);
-  //     } catch (error) {
-  //       console.error(error);
-  //       setIsAuthenticated(false);
-  //     }
-  //   };
-  //   refreshAuthToken();
-  // }, []);
+  useEffect(() => {
+    setIsLoading(true);
+    const refreshAuthToken = async () => {
+      if (!isAuthenticated) {
+        try {
+          await axios.post(
+            `${API_URL}/auth/refresh-token`,
+            {},
+            {
+              withCredentials: true,
+            }
+          );
+          setIsAuthenticated(true);
+        } catch (error) {
+          console.error(error);
+          setIsAuthenticated(false);
+        }
+      }
+    };
+    refreshAuthToken();
+    setIsLoading(false);
+  }, [navigate]);
   return (
-    <header className="flex py-1 pl-3 pr-2 bg-white dark:bg-[#242526] items-center sticky top-0 transition-colors duration-500 shadow-md z-40">
-      <section className="basis-1/4">
+    <header className="flex justify-end sm:justify-normal py-1 pl-3 pr-2 bg-white dark:bg-[#242526] items-center sticky top-0 transition-colors duration-500 shadow-md z-40">
+      <section className="basis-1/4 absolute left-0 sm:relative">
         <SearchBar />
       </section>
       <section className="basis-1/2 text-center">
-        <a href="/">
+        <Link to={"/"}>
           <span className="material-symbols-outlined dark:text-white">
             token
           </span>
-        </a>
+        </Link>
       </section>
 
       <section className="flex basis-1/4 justify-end items-center gap-2">
-        {console.log("isAuthenticated", isAuthenticated)}
-        {isAuthenticated ? (
+        {isLoading ? (
+          <SkeletonLoader
+            width="7rem"
+            height="3rem"
+            className="dark:bg-[#fafafa] rounded-full"
+          />
+        ) : isAuthenticated ? (
           <ProfileMenu />
         ) : (
           <div className="flex gap-2 py-1">
-            <Button onClick={() => navigate("/signup")}>Sign up</Button>
+            <Button
+              className={"hidden sm:block"}
+              onClick={() => navigate("/signup")}
+            >
+              Sign up
+            </Button>
             <Button
               onClick={() => navigate("/login")}
               className="bg-blue-500 hover:bg-blue-600 dark:bg-indigo-500 dark:hover:bg-indigo-600 text-white"
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,100 +1,116 @@
 import { Bookmark, Home, UserCheck, Users } from "lucide-react";
+import { useState } from "react";
 
 export function Sidebar({ variant, setCurrentPage }) {
   const selectColor = "#0866ff";
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
   return (
-    <section className="  basis-1/4 h-screen sticky top-[4.5rem] overflow-y-auto rounded-md gap-1 max-w-80">
-      <div
-        onClick={() => {
-          if (variant !== "home") setCurrentPage("home");
-        }}
-        className={`flex items-center gap-2 p-3 cursor-pointer rounded-md ${
-          variant !== "home" ? "hover:bg-[#e3e5e9] dark:hover:bg-[#414141]" : ""
-        }`}
-      >
-        {variant === "home" ? (
-          <svg viewBox="0 0 24 24" width="24" height="24" fill={selectColor}>
-            <path d="M9.464 1.286C10.294.803 11.092.5 12 .5c.908 0 1.707.303 2.537.786.795.462 1.7 1.142 2.815 1.977l2.232 1.675c1.391 1.042 2.359 1.766 2.888 2.826.53 1.059.53 2.268.528 4.006v4.3c0 1.355 0 2.471-.119 3.355-.124.928-.396 1.747-1.052 2.403-.657.657-1.476.928-2.404 1.053-.884.119-2 .119-3.354.119H7.93c-1.354 0-2.471 0-3.355-.119-.928-.125-1.747-.396-2.403-1.053-.656-.656-.928-1.475-1.053-2.403C1 18.541 1 17.425 1 16.07v-4.3c0-1.738-.002-2.947.528-4.006.53-1.06 1.497-1.784 2.888-2.826L6.65 3.263c1.114-.835 2.02-1.515 2.815-1.977zM10.5 13A1.5 1.5 0 0 0 9 14.5V21h6v-6.5a1.5 1.5 0 0 0-1.5-1.5h-3z"></path>
-          </svg>
-        ) : (
-          <Home />
-        )}
-        <span
-          className={
-            variant === "home" ? `font-bold text-[${selectColor}]` : ""
-          }
+    <section
+      className={`flex sm:flex-col justify-around sm:justify-normal basis-1/4 sm:h-screen lg:sticky top-[4.5rem] overflow-y-auto rounded-md gap-1 sm:max-w-80 transition-transform ${
+        isSidebarVisible ? "translate-x-0" : "-translate-x-0"
+      } lg:translate-x-0`}
+    >
+      <div className={`flex`}>
+        <div
+          onClick={() => {
+            if (variant !== "home") setCurrentPage("home");
+          }}
+          className={`flex items-center gap-2 py-3 px-4 cursor-pointer rounded-full ${
+            variant !== "home"
+              ? "hover:bg-[#e3e5e9] dark:hover:bg-[#414141]"
+              : "dark:bg-blue-950 bg-blue-200"
+          }`}
         >
-          Home
-        </span>
+          {variant === "home" ? (
+            <svg viewBox="0 0 24 24" width="24" height="24" fill={selectColor}>
+              <path d="M9.464 1.286C10.294.803 11.092.5 12 .5c.908 0 1.707.303 2.537.786.795.462 1.7 1.142 2.815 1.977l2.232 1.675c1.391 1.042 2.359 1.766 2.888 2.826.53 1.059.53 2.268.528 4.006v4.3c0 1.355 0 2.471-.119 3.355-.124.928-.396 1.747-1.052 2.403-.657.657-1.476.928-2.404 1.053-.884.119-2 .119-3.354.119H7.93c-1.354 0-2.471 0-3.355-.119-.928-.125-1.747-.396-2.403-1.053-.656-.656-.928-1.475-1.053-2.403C1 18.541 1 17.425 1 16.07v-4.3c0-1.738-.002-2.947.528-4.006.53-1.06 1.497-1.784 2.888-2.826L6.65 3.263c1.114-.835 2.02-1.515 2.815-1.977zM10.5 13A1.5 1.5 0 0 0 9 14.5V21h6v-6.5a1.5 1.5 0 0 0-1.5-1.5h-3z"></path>
+            </svg>
+          ) : (
+            <Home />
+          )}
+          <span
+            className={`${
+              variant === "home" ? `font-bold text-[${selectColor}]` : ""
+            } hidden sm:block`}
+          >
+            Home
+          </span>
+        </div>
       </div>
-      <div
-        onClick={() => {
-          if (variant !== "followers") setCurrentPage("followers");
-        }}
-        className={`flex items-center gap-2 p-3 cursor-pointer rounded-md ${
-          variant !== "followers"
-            ? "hover:bg-[#e3e5e9] dark:hover:bg-[#414141]"
-            : ""
-        }`}
-      >
-        {variant === "followers" ? (
-          <Users fill={selectColor} stroke={selectColor} />
-        ) : (
-          <Users />
-        )}
-        <span
-          className={
-            variant === "followers" ? `font-bold text-[${selectColor}]` : ""
-          }
+      <div className={`flex`}>
+        <div
+          onClick={() => {
+            if (variant !== "followers") setCurrentPage("followers");
+          }}
+          className={`flex items-center gap-2 py-3 px-4 cursor-pointer rounded-full ${
+            variant !== "followers"
+              ? "hover:bg-[#e3e5e9] dark:hover:bg-[#414141]"
+              : "dark:bg-blue-950 bg-blue-200"
+          }`}
         >
-          Followers
-        </span>
+          {variant === "followers" ? (
+            <Users fill={selectColor} stroke={selectColor} />
+          ) : (
+            <Users />
+          )}
+          <span
+            className={`${
+              variant === "followers" ? `font-bold text-[${selectColor}]` : ""
+            } hidden sm:block`}
+          >
+            Followers
+          </span>
+        </div>
       </div>
-      <div
-        onClick={() => {
-          if (variant !== "following") setCurrentPage("following");
-        }}
-        className={`flex items-center gap-2 p-3 cursor-pointer rounded-md ${
-          variant !== "following"
-            ? "hover:bg-[#e3e5e9] dark:hover:bg-[#414141]"
-            : ""
-        }`}
-      >
-        {variant === "following" ? (
-          <UserCheck fill={selectColor} stroke={selectColor} />
-        ) : (
-          <UserCheck />
-        )}
-        <span
-          className={
-            variant === "following" ? `font-bold text-[${selectColor}]` : ""
-          }
+      <div className={`flex`}>
+        <div
+          onClick={() => {
+            if (variant !== "following") setCurrentPage("following");
+          }}
+          className={`flex items-center gap-2 py-3 px-4 cursor-pointer rounded-full ${
+            variant !== "following"
+              ? "hover:bg-[#e3e5e9] dark:hover:bg-[#414141]"
+              : "dark:bg-blue-950 bg-blue-200"
+          }`}
         >
-          Following
-        </span>
+          {variant === "following" ? (
+            <UserCheck fill={selectColor} stroke={selectColor} />
+          ) : (
+            <UserCheck />
+          )}
+          <span
+            className={`${
+              variant === "following" ? `font-bold text-[${selectColor}]` : ""
+            } hidden sm:block`}
+          >
+            Following
+          </span>
+        </div>
       </div>
-      <div
-        onClick={() => {
-          if (variant !== "saved") setCurrentPage("saved");
-        }}
-        className={`flex items-center gap-2 p-3 cursor-pointer rounded-md ${
-          variant !== "saved"
-            ? "hover:bg-[#e3e5e9] dark:hover:bg-[#414141]"
-            : ""
-        }`}
-      >
-        {variant === "saved" ? (
-          <Bookmark fill={selectColor} stroke={selectColor} />
-        ) : (
-          <Bookmark />
-        )}
-        <span
-          className={
-            variant === "saved" ? `font-bold text-[${selectColor}]` : ""
-          }
+      <div className={`flex`}>
+        <div
+          onClick={() => {
+            if (variant !== "saved") setCurrentPage("saved");
+          }}
+          className={`flex items-center gap-2 py-3 px-4 cursor-pointer rounded-full ${
+            variant !== "saved"
+              ? "hover:bg-[#e3e5e9] dark:hover:bg-[#414141]"
+              : "dark:bg-blue-950 bg-blue-200"
+          }`}
         >
-          Saved
-        </span>
+          {variant === "saved" ? (
+            <Bookmark fill={selectColor} stroke={selectColor} />
+          ) : (
+            <Bookmark />
+          )}
+          <span
+            className={`${
+              variant === "saved" ? `font-bold text-[${selectColor}]` : ""
+            } hidden sm:block`}
+          >
+            Saved
+          </span>
+        </div>
       </div>
     </section>
   );
