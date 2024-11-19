369f9b6da103d307ef507940e6dfb55355dfffec
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-11-19 13:30:30 +0530

    Route changes: for reset and change password

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,12 +7,19 @@ const {
   verifyEmail,
   logout,
   refreshAccessToken,
+  resetPassword,
+  changePassword,
 } = require("../controllers/auth");
 const auth = require("../middleware/authentication");
+const { verifyResetPassword } = require("../controllers/user");
+const decryptUrl = require("../middleware/url-Decryption");
 
 router.route("/signup").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/user/verify-email").get(verifyEmail);
+router.route("/user/reset-password").get(verifyResetPassword);
+router.route("/reset-password/:id").post(decryptUrl, resetPassword);
+router.route("/user/change-password").post(auth, changePassword);
 
 router.route("/refresh-token").post(auth, refreshAccessToken);
 router.route("/logout").post(auth, logout);
