c2223973914c40d3112d00dad9dc8c7336229b2c
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-11-20 01:18:40 +0530

    minor change

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -134,6 +134,7 @@ export function SignupPage() {
                 }}
                 id="date_of_birth"
                 name="date_of_birth"
+                placeholder="Enter your date of birth"
                 type="date"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
