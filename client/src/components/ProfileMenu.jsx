6f556f9d7af7cffac2d9f5ca6d8424a501c9fc65
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-11-22 21:43:21 +0530

    Fixed: unhandled undefined value

--- client/src/components/ProfileMenu.jsx
+++ client/src/components/ProfileMenu.jsx
@@ -24,11 +24,18 @@ import useTheme from "../contexts/theme";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useEffect } from "react";
 
 export function ProfileMenu({ setPageLoading }) {
   const { theme, darkTheme, lightTheme } = useTheme();
-  const { isAuthenticated } = useAuth();
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.name) {
+      handleLogout();
+    }
+  }, [navigate]);
 
   const switchTheme = (e) => {
     if (theme === "white") {
@@ -56,12 +63,12 @@ export function ProfileMenu({ setPageLoading }) {
 
   return (
     <>
-      {isAuthenticated && (
+      {localStorage.name && (
         <DropdownMenu modal={false}>
           <DropdownMenuTrigger asChild>
             <div className="flex items-center gap-2 cursor-pointer hover:bg-[#e3e5e9] active:bg-[#d0d2d6] dark:hover:bg-[#414141] dark:active:bg-[#313131] active:scale-95 select-none px-1 sm:pl-3 sm:pr-1 py-1 rounded-full">
               <span className="dark:text-white bg-inherit hover:bg-inherit font-medium hidden sm:block">
-                {localStorage.name?.split(" ")[0]}
+                {localStorage.name.split(" ")[0]}
               </span>
               <Avatar>
                 <AvatarImage src={localStorage.avatar} />
