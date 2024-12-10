e678ed95c0e01e24337223971b5ae492e1d89045
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2024-12-10 12:24:04 +0530

    minor change

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -309,8 +309,10 @@ export function ProfilePage() {
                             <h1 className="text-2xl font-bold w-auto">
                               {`${details.user[0].name}`}
                             </h1>
-                            {details.user[0]._id ===
-                              "673322bc9aec9c0245313129" && (
+                            {(details.user[0]._id ===
+                              "673322bc9aec9c0245313129" ||
+                              details.user[0]._id ===
+                                "6757df6530cc68ed2c3e9caa") && (
                               <div className="mb-1">
                                 <svg
                                   aria-label="Verified"
@@ -321,7 +323,7 @@ export function ProfilePage() {
                                   viewBox="0 0 40 40"
                                   width="18"
                                 >
-                                  <title>One of the creator of this app</title>
+                                  {/* <title>One of the creator of this app</title> */}
                                   <path
                                     d="M19.998 3.094 14.638 0l-2.972 5.15H5.432v6.354L0 14.64 3.094 20 0 25.359l5.432 3.137v5.905h5.975L14.638 40l5.36-3.094L25.358 40l3.232-5.6h6.162v-6.01L40 25.359 36.905 20 40 14.641l-5.248-3.03v-6.46h-6.419L25.358 0l-5.36 3.094Zm7.415 11.225 2.254 2.287-11.43 11.5-6.835-6.93 2.244-2.258 4.587 4.581 9.18-9.18Z"
                                     fillRule="evenodd"
