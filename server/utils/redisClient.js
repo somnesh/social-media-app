663c324d3af2f81be919630c80f924e8f75ea887
Somnesh Mukhopadhyay <86874926+somnesh@users.noreply.github.com>
2025-10-28 00:30:21 +0530

    feat: add Redis client setup with connection and error handling

---
+++ server/utils/redisClient.js
@@ -0,0 +1,8 @@
+const { Redis } = require("ioredis");
+
+const redisClient = new Redis(process.env.REDIS_URL);
+
+redisClient.on("connect", () => console.log("Connected to Redis"));
+redisClient.on("error", (err) => console.error("Redis error:", err));
+
+module.exports = redisClient;
