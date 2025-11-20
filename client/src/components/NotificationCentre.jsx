8e00e8f09fc5970190b01c76ef1dcf8fceb280e1
Somnesh Mukhopadhyay <86874926+somnesh@users.noreply.github.com>
2025-11-20 19:50:52 +0530

    feat: refactor NotificationCentre to remove Accordion component for simplified notification display

--- client/src/components/NotificationCentre.jsx
+++ client/src/components/NotificationCentre.jsx
@@ -1,11 +1,3 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-
-import { Bell } from "lucide-react";
 import { NotificationContent } from "./NotificationContent";
 
 import useNotifications from "./hooks/useNotifications";
@@ -24,44 +16,21 @@ export function NotificationCentre({ mobileFlag }) {
               <span className="text-2xl font-bold">Notifications</span>
             </div>
           )}
-          <Accordion
-            type="single"
-            defaultValue={mobileFlag ? "item-1" : ""}
-            collapsible={!mobileFlag}
-          >
-            <AccordionItem value="item-1">
-              <AccordionTrigger
-                className={
-                  "bg-[#ffffff] dark:bg-[#303030] hover:bg-[#faf8f8] dark:hover:bg-[#535353] dark:active:bg-[#303030] p-3 rounded-t-md hidden sm:flex"
-                }
-              >
-                <div className="flex items-center gap-2">
-                  <Bell />
-                  Notifications
-                </div>
-              </AccordionTrigger>
-              <AccordionContent
-                className={
-                  "bg-white dark:bg-[#242526] rounded-b-md h-auto sm:h-auto  sm:pb-1"
-                }
-              >
-                <div className="sm:max-h-96 h-auto overflow-y-auto">
-                  {notifications.length !== 0 ? (
-                    notifications.map((notification, index) => (
-                      <NotificationContent
-                        key={notification._id}
-                        message={notification}
-                      />
-                    ))
-                  ) : (
-                    <span className="block p-3 font-medium w-full text-center">
-                      Nothing here right now.
-                    </span>
-                  )}
-                </div>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
+
+          <div className="sm:max-h-screen h-auto overflow-y-auto">
+            {notifications.length !== 0 ? (
+              notifications.map((notification, index) => (
+                <NotificationContent
+                  key={notification._id}
+                  message={notification}
+                />
+              ))
+            ) : (
+              <span className="block p-3 font-medium w-full text-center">
+                Nothing here right now.
+              </span>
+            )}
+          </div>
         </>
       )}
     </div>
