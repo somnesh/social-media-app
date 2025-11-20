0dbf7e8dc4f6785127087e9f45c0e0756cf8feeb
Somnesh Mukhopadhyay <86874926+somnesh@users.noreply.github.com>
2025-11-20 19:40:46 +0530

    feat: refactor Tailwind CSS configuration and styles for improved structure and compatibility

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -34,6 +35,7 @@ export default defineConfig({
       },
     }),
     react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
