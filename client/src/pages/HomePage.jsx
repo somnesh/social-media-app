6369f803b942e346a2754e84d11e5fff12b2db4f
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2025-12-04 00:07:12 +0530

    refactor HomePage layout and integrate Avatar components for improved user experience

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
 import { Feed } from "../components/Feed";
 import { Header } from "../components/Header";
-import { Sidebar } from "../components/Sidebar";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { WholePageLoader } from "../components/loaders/WholePageLoader";
 import { useAuth } from "../contexts/AuthContext";
 import { NotificationCentre } from "../components/NotificationCentre";
@@ -11,6 +10,8 @@ import { SavedPosts } from "../components/SavedPosts";
 import { FollowerList } from "../components/FollowerList";
 import { FollowingList } from "../components/FollowingList";
 import ChatPage from "./Chat";
+import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
+import { BadgeHelp, Bookmark, Cake, Search, Settings } from "lucide-react";
 
 export const Home = () => {
   const { isAuthenticated, setIsAuthenticated } = useAuth();
@@ -71,11 +72,59 @@ export const Home = () => {
         <WholePageLoader />
       ) : (
         <>
-          <Header setPageLoading={setPageLoading} />
-          <main className="flex flex-col relative sm:justify-between lg:px-2 sm:py-4 md:pr-2 bg-white dark:bg-black h-fit dark:text-[#e2e4e9] transition-colors duration-500 sm:gap-3">
-            <Sidebar variant={currentPage} setCurrentPage={setCurrentPage} />
-            {/* --------------------------------------- */}
-            <section className="lg:basis-1/2 md:basis-3/4 sm:px-1 sm:mx-auto max-w-[680px] overflow-y-auto w-full">
+          <div className="sm:hidden flex justify-center w-auto h-10 my-2">
+            <div className="flex  items-center">
+              <span className="leckerli-one-regular text-foreground text-xl">
+                Connectify
+              </span>
+            </div>
+          </div>
+          <Header
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            setPageLoading={setPageLoading}
+          />
+          {/* <Sidebar variant={currentPage} setCurrentPage={setCurrentPage} /> */}
+          <main className="flex relative sm:justify-between lg:px-2 sm:py-3 md:pr-2 bg-white dark:bg-black h-fit dark:text-[#e2e4e9] transition-colors duration-500 sm:gap-3">
+            {/* ================================================================ */}
+
+            <section className="lg:basis-1/3 md:basis-1/4 hidden lg:block lg:sticky md:sticky top-22 h-fit max-w-[320px] overflow-y-auto">
+              <div className="h-full w-full flex flex-col border rounded-2xl p-2 gap-1">
+                <div className="relative w-auto flex items-center gap-2 px-3 py-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer">
+                  <Avatar>
+                    <AvatarImage src={localStorage.avatar} />
+                    <AvatarFallback className={localStorage.avatarBg}>
+                      {localStorage.name[0]}
+                    </AvatarFallback>
+                  </Avatar>
+                  <span className="font-bold">{localStorage.name}</span>
+                </div>
+                <div className="flex px-3 py-3 gap-3 items-center rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer">
+                  <Search size={20} />
+                  <span className="text-sm">Search</span>
+                </div>
+                <div className="flex px-3 py-3 gap-3 items-center rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer">
+                  <Bookmark size={20} />
+                  <span className="text-sm">Saved Posts</span>
+                </div>
+                <div className="flex px-3 py-3 gap-3 items-center rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer">
+                  <Cake size={20} />
+                  <span className="text-sm">Birthdays</span>
+                </div>
+                <div className="flex px-3 py-3 gap-3 items-center rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer">
+                  <Settings size={20} />
+                  <span className="text-sm">Settings</span>
+                </div>
+                <div className="flex px-3 py-3 gap-3 items-center rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer">
+                  <BadgeHelp size={20} />
+                  <span className="text-sm">Help & Support</span>
+                </div>
+              </div>
+            </section>
+
+            {/* ================================================================ */}
+
+            <section className="lg:basis-2/3 md:basis-3/4 sm:px-1 max-w-[680px] overflow-y-auto w-full mt-[3px]">
               {currentPage === "home" && <Feed />}
               {currentPage === "followers" && <FollowerList />}
               {currentPage === "following" && <FollowingList />}
@@ -85,11 +134,72 @@ export const Home = () => {
                 <NotificationCentre mobileFlag={true} />
               )}
             </section>
+
+            {/* ================================================================ */}
+
+            <section className="lg:basis-1/3 md:basis-1/4 hidden lg:block lg:sticky md:sticky top-22 h-fit max-w-[320px] overflow-y-auto">
+              <div className="flex flex-col h-full w-full border rounded-2xl pb-2">
+                <span className="text-xl font-bold m-4">Friends</span>
+                <div className="flex flex-col gap-1 px-2">
+                  <div className="relative w-auto flex items-center gap-2 px-3 py-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer">
+                    <Avatar>
+                      <AvatarImage src={localStorage.avatar} />
+                      <AvatarFallback className={localStorage.avatarBg}>
+                        {localStorage.name[0]}
+                      </AvatarFallback>
+                    </Avatar>
+                    <span className="font-semibold text-sm">
+                      {localStorage.name}
+                    </span>
+                  </div>
+                  <div className="relative w-auto flex items-center gap-2 px-3 py-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer">
+                    <Avatar>
+                      <AvatarImage src={localStorage.avatar} />
+                      <AvatarFallback className={localStorage.avatarBg}>
+                        {localStorage.name[0]}
+                      </AvatarFallback>
+                    </Avatar>
+                    <span className="font-semibold text-sm">
+                      {localStorage.name}
+                    </span>
+                  </div>
+                  <div className="relative w-auto flex items-center gap-2 px-3 py-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer">
+                    <Avatar>
+                      <AvatarImage src={localStorage.avatar} />
+                      <AvatarFallback className={localStorage.avatarBg}>
+                        {localStorage.name[0]}
+                      </AvatarFallback>
+                    </Avatar>
+                    <span className="font-semibold text-sm">
+                      {localStorage.name}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </section>
+
+            {/* ================================================================ */}
+
+            <div className="lg:max-w-fit hidden mt-10 fixed bottom-1 lg:flex gap-2 text-xs text-muted-foreground">
+              <Link to={"/terms"} className="hover:underline">
+                Terms
+              </Link>
+              <span> · </span>
+              {/* <Link to={"/community-guidelines"}>Community Guidelines</Link> */}
+              {/* <Link to={"/data-deletion-policy"}>Data Deletion Policy</Link> */}
+              <Link to={"/cookie-policy"} className="hover:underline">
+                Cookies
+              </Link>
+              <span> · </span>
+              <Link to={"/privacy-policy"} className="hover:underline">
+                Privacy
+              </Link>
+            </div>
             {/* --------------------------------------- */}
             {/* <section
               className={`basis-1/4 h-fit max-w-96 lg:sticky md:sticky absolute top-18 overflow-y-auto transition-transform hidden sm:block -translate-y-full md:translate-y-0`}
             >
-              <NotificationCentre />
+              
             </section> */}
           </main>
         </>
